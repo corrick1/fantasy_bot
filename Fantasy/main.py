6b8c036fb8297775819593d01309dbc4bc16c601
onelock <108121531+corrick1@users.noreply.github.com>
2024-05-16 22:17:59 +0300

    Update main.py

--- Fantasy/main.py
+++ Fantasy/main.py
@@ -2,7 +2,7 @@ from fantasy_api import FantasyAPI
 from fantasy_bot import FantasyBot
 
 if __name__ == '__main__':
-    fantasy_token_file = 'YOUR_PATH'
+    fantasy_token_file = 'C:\\Users\\vitia\\OneDrive\\Рабочий стол\\MAIN_\\soft_\\telegram_fantasy\\token_one.txt'
     fantasy_api = FantasyAPI(fantasy_token_file)
     bot = FantasyBot('YOUR_TELEGRAM_BOT_TOKEN', fantasy_api)  # Замените 'YOUR_TELEGRAM_BOT_TOKEN' на ваш настоящий токен
-    bot.start_polling()
+    bot.updater.start_polling()
