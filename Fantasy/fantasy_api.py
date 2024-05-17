490396479dbeefa120b3ffd1ff7f0bb784d671a2
onelock <108121531+corrick1@users.noreply.github.com>
2024-05-17 11:49:14 +0300

    Update fantasy_api.py

--- Fantasy/fantasy_api.py
+++ Fantasy/fantasy_api.py
@@ -19,7 +19,19 @@ class FantasyAPI:
             url = "https://api.fantasy.top/v1/graphql"
             headers = {
                 "Content-Type": "application/json",
-                "Authorization": f"Bearer {token}"
+                "Authorization": f"Bearer {token}",
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                "Origin": "https://www.fantasy.top",
+                "Referer": "https://www.fantasy.top/",
+                "Sec-Ch-Ua": "\"Google Chrome\";v=\"123\", \"Not:A-Brand\";v=\"8\", \"Chromium\";v=\"123\"",
+                "Sec-Ch-Ua-Mobile": "?0",
+                "Sec-Ch-Ua-Platform": "\"Linux\"",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-site",
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
             }
 
             payload = {
